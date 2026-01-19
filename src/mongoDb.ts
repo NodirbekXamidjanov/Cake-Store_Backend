61d6b5f6560205c99d898eb2458523dabb77dfa5
unknown <xamidjonovnodirbek@gmail.com>
2026-01-19 20:42:11 +0500

    cake-store-backend-finalized-v1.0.1

--- src/mongoDb.ts
+++ src/mongoDb.ts
@@ -1,6 +1,6 @@
 import { MongoClient, Db, Collection, ObjectId } from 'mongodb';
 
-const MONGODB_URI = process.env.MONGODB_URI!
+const MONGODB_URI = process.env.MONGODB_URI! as string
 const DB_NAME = 'cakes_db';
 
 let db: Db;
