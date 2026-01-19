80f9ba2e8cdbf9b6aac3e59c4b2f7aa2898bc0e4
unknown <xamidjonovnodirbek@gmail.com>
2026-01-19 21:05:17 +0500

    cake-store-backend-finalized-v1.0.1

--- src/mongoDb.ts
+++ src/mongoDb.ts
@@ -1,27 +1,31 @@
-import { MongoClient, Db, Collection, ObjectId } from 'mongodb';
+import { MongoClient, Db, Collection } from 'mongodb';
 
-const MONGODB_URI = process.env.MONGODB_URI! as string
+const MONGODB_URI = process.env.MONGODB_URI! as string;
 const DB_NAME = 'cakes_db';
 
 let db: Db;
 let cakesCollection: Collection;
 
 export async function connectDB() {
-	try {
-		const client = new MongoClient(MONGODB_URI);
-		await client.connect();
-		console.log('MongoDB ga ulanish muvaffaqiyatli!');
-		
-		db = client.db(DB_NAME);
-		cakesCollection = db.collection('cakes');
-		
-		return db;
-	} catch (error) {
-		console.error('MongoDB ulanish xatosi:', error);
-		process.exit(1);
-	}
+    try {
+        const client = new MongoClient(MONGODB_URI, {
+            tls: true,                       // SSL ulanish
+            serverSelectionTimeoutMS: 5000,  // 5 sekund kutish
+        });
+
+        await client.connect();
+        console.log('MongoDB ga ulanish muvaffaqiyatli!');
+
+        db = client.db(DB_NAME);
+        cakesCollection = db.collection('cakes');
+
+        return db;
+    } catch (error) {
+        console.error('MongoDB ulanish xatosi:', error);
+        process.exit(1);
+    }
 }
 
 export function getCakesCollection() {
-	return cakesCollection;
+    return cakesCollection;
 }
